892bb6510afa3dd1921a64bd2c5facd2dad78848
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-12 13:43:51 +0000

    push

--- app/page.jsx
+++ app/page.jsx
@@ -210,7 +210,7 @@ export default function Home() {
             <div className="spline-container">
               <div className="spline-border-frame">
                 <Spline
-                  scene="https://prod.spline.design/HxOEAT7QxoE-wK4X/scene.splinecode"
+                  scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
                   className="w-screen h-screen rounded-lg"
                 />
                 
