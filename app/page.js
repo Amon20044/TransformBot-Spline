bde1f8ab82bfd9a060460dbb6646b310fef883ed
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-14 00:02:16 +0000

    push

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-center min-w-screen text-white">
       <Spline
         scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode" 
       />
