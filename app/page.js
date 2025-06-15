475054e3b99890ba749c84b028812d0ed99c383a
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-15 13:49:42 +0000

    Ok

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Spline from '@splinetool/react-spline/next';
-import  OptimizedLoader from './components/LoaderClient';
 
 export default function Home() {
   return (
-      <div className="spline-container" id="splineContainer">
-        <Spline
-          scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
-          className="absolute top-0 left-0 w-full h-full"
-        />    
-        </div>
+    <main>
+      <Spline
+        scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode" 
+      />
+    </main>
   );
 }
