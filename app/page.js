9884eb5eb989378de4744f81420fd47683efe374
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-15 22:56:52 +0000

    s

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="relative w-screen bg-[#fffaef] h-screen overflow-x-hidden">
       {/* Fixed Centered Spline Card */}
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[97vw] h-[98vh] z-10 rounded-3xl overflow-hidden shadow-xl border border-black bg-white">
+      <div className="w-screen h-screen">
         <Spline
           scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
           className="w-full h-full"
