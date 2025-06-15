2fdf513e4274c0361c8cf37924ea2dc1affc4199
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-15 21:26:42 +0000

    push

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import  OptimizedLoader from './components/LoaderClient';
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
+    <div className="relative w-screen">
       {/* Client-side Loader Component */}
       <OptimizedLoader />
 
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="spline-container" id="splineContainer">
         <Spline
           scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
-          className="absolute top-0 left-0 w-full h-full"
+          className="left-0 w-screen h-screen"
         />
       </div>
     </div>
