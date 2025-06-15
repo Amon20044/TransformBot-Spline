20e32b7d8887874ea75f8b45edd4ddccc68799c8
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-15 22:25:51 +0000

    okkk

--- app/page.js
+++ app/page.js
@@ -1,16 +1,23 @@
+"use client"
 import Spline from '@splinetool/react-spline/next';
-import OptimizedLoader from './components/LoaderClient';
+
+function loader(){
+  return(
+    <h3 className="text-2xl text-center text-gray-700 font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100]">
+      Please wait while the 3D scene loads...
+    </h3>
+  )
+}
 
 export default function Home() {
+  
   return (
-    <div className="relative w-screen bg-[#fffaef] min-h-[5000px] max-md:min-h-[12000px] overflow-x-hidden">
-      {/* Client-side Loader Component */}
-      <OptimizedLoader />
-
+    <div className="relative w-screen bg-[#fffaef] h-screen overflow-x-hidden">
       {/* Fixed Centered Spline Card */}
       <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[97vw] h-[98vh] z-10 rounded-3xl overflow-hidden shadow-xl border border-black bg-white">
         <Spline
           scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
+          onLoad={loader}
           className="w-full h-full"
         />
       </div>
