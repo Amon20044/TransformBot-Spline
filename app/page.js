2f857093e31c8a1a7a50e9eb125583e239e2941f
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-06-15 22:26:23 +0000

    d

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
-"use client"
-import Spline from '@splinetool/react-spline/next';
 
-function loader(){
-  return(
-    <h3 className="text-2xl text-center text-gray-700 font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100]">
-      Please wait while the 3D scene loads...
-    </h3>
-  )
+import Spline from '@splinetool/react-spline/next';
 }
 
 export default function Home() {
@@ -17,7 +10,6 @@ export default function Home() {
       <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[97vw] h-[98vh] z-10 rounded-3xl overflow-hidden shadow-xl border border-black bg-white">
         <Spline
           scene="https://prod.spline.design/no2TF-PIICSPnVho/scene.splinecode"
-          onLoad={loader}
           className="w-full h-full"
         />
       </div>
